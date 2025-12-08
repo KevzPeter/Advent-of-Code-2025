c5af1899c5f2ee7dac6af5ebb1d7ea59f0c896cc
KevzPeter <ultimatekevin1@gmail.com>
2025-12-08 02:40:22 -0500

    day 4 solutions

---
+++ Dec_4/solution_2.py
@@ -0,0 +1,51 @@
+from os import path
+
+sample_input_file = path.join(path.dirname(__file__), 'sample_input.txt')
+input_file = path.join(path.dirname(__file__), 'input.txt')
+
+with open(input_file, 'r') as file:
+    data = file.read().split('\n')
+
+
+def create_grid(data):
+    grid = []
+    for line in data:
+        grid.append([x for x in line])
+    return grid
+
+
+grid = create_grid(data)
+
+m = len(grid)
+n = len(grid[0])
+
+dirs = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+
+def is_valid(i, j):
+    return 0 <= i < m and 0 <= j < n
+
+
+def getRolls() -> int:
+    rolls_taken = 0
+    rolls_taken_coords = []
+    for i in range(m):
+        for j in range(n):
+            if (grid[i][j] != '@'):
+                continue
+            adjacent_rolls = 0
+            for dir in dirs:
+                x, y = i + dir[0], j + dir[1]
+                if is_valid(x, y) and grid[x][y] == '@':
+                    adjacent_rolls += 1
+            if (adjacent_rolls < 4):
+                rolls_taken_coords.append((i, j))
+                rolls_taken += 1
+    for i, j in rolls_taken_coords:
+        grid[i][j] = '.'
+    return rolls_taken
+
+
+total_rolls = sum(iter(lambda: getRolls(), 0))
+
+print(total_rolls)
