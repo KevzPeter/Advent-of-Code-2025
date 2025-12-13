8cf3243226be3fd9a44c3d90626da7a4b09b3531
KevzPeter <ultimatekevin1@gmail.com>
2025-12-13 05:55:41 -0500

    Day 11 Part 1 solution

---
+++ Dec_11/solution_1.py
@@ -0,0 +1,77 @@
+'''
+Advent of Code - Dec 11
+Author: Kevin Peter
+--- Day 11: Reactor ---
+You hear some loud beeping coming from a hatch in the floor of the factory, so you decide to check it out. Inside, you find several large electrical conduits and a ladder.
+
+Climbing down the ladder, you discover the source of the beeping: a large, toroidal reactor which powers the factory above. Some Elves here are hurriedly running between the reactor and a nearby server rack, apparently trying to fix something.
+
+One of the Elves notices you and rushes over. "It's a good thing you're here! We just installed a new server rack, but we aren't having any luck getting the reactor to communicate with it!" You glance around the room and see a tangle of cables and devices running from the server rack to the reactor. She rushes off, returning a moment later with a list of the devices and their outputs (your puzzle input).
+
+For example:
+
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+Each line gives the name of a device followed by a list of the devices to which its outputs are attached. So, bbb: ddd eee means that device bbb has two outputs, one leading to device ddd and the other leading to device eee.
+
+The Elves are pretty sure that the issue isn't due to any specific device, but rather that the issue is triggered by data following some specific path through the devices. Data only ever flows from a device through its outputs; it can't flow backwards.
+
+After dividing up the work, the Elves would like you to focus on the devices starting with the one next to you (an Elf hastily attaches a label which just says you) and ending with the main output to the reactor (which is the device with the label out).
+
+To help the Elves figure out which path is causing the issue, they need you to find every path from you to out.
+
+In this example, these are all of the paths from you to out:
+
+Data could take the connection from you to bbb, then from bbb to ddd, then from ddd to ggg, then from ggg to out.
+Data could take the connection to bbb, then to eee, then to out.
+Data could go to ccc, then ddd, then ggg, then out.
+Data could go to ccc, then eee, then out.
+Data could go to ccc, then fff, then out.
+In total, there are 5 different paths leading from you to out.
+
+How many different paths lead from you to out?
+'''
+from os import path
+import re
+
+sample_input_file = path.join(path.dirname(__file__), 'sample_input.txt')
+input_file = path.join(path.dirname(__file__), 'input.txt')
+
+with open(input_file, 'r') as file:
+    data = file.read().splitlines()
+
+adj_list = {}
+
+
+def create_adj_list(data):
+    for line in data:
+        nodes, edges = re.split(r': ', line)
+        adj_list[nodes] = edges.split(' ')
+    return adj_list
+
+
+create_adj_list(data)
+
+
+def find_all_paths(adj_list, start, end, visited) -> int:
+    if (start == end):
+        return 1
+    visited.add(start)  # not really needed since no cycles in the graph
+    paths = 0
+    for node in adj_list[start]:
+        if (node not in visited):
+            paths += find_all_paths(adj_list, node, end, visited)
+    visited.remove(start)
+    return paths
+
+
+paths = find_all_paths(adj_list, 'you', 'out', visited=set())
+print(paths)
